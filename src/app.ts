af325b8dba3fcabc4ed4b18bbab964c151cd2077
Alexandre Wotawa <awotawa@gmail.com>
2023-05-04 14:59:02 +0200

    feat: configure MongoDB db do not foget to add in a nodemon.json file your MongoDB credentials

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import astronautRoutes from "./routes";
+
+const app: Express = express();
+
+const PORT: string | number = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(astronautRoutes);
+
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.q1vb3qh.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  autoIndex: true,
+};
+
+mongoose
+  .connect(uri, options)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => {
+    throw error;
+  });
